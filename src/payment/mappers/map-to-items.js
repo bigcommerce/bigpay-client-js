d086bdaa4bc9cb7c0ce8ce33fab00fa9e9121bd6
David Chin <davidchin.yf@gmail.com>
2016-10-04 15:51:45 +1100

    CHECKOUT-973: Add amount fallback

--- src/payment/mappers/map-to-items.js
+++ src/payment/mappers/map-to-items.js
@@ -8,10 +8,11 @@ import { omitNil } from '../../common/utils';
 export default function mapToItems(data) {
     const { cart = { items: [] } } = data;
 
+    // KLUDGE: amount * 100 - integerAmount is not available yet
     return cart.items.map(itemData => omitNil({
         code: itemData.id,
         name: itemData.name,
-        price: itemData.integerAmount,
+        price: itemData.integerAmount || itemData.amount * 100,
         quantity: itemData.quantity,
         sku: itemData.sku,
     }));
