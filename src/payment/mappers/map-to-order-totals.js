f030e8d1677bb0057ec4a9d6ee9dcf44237a87c5
David Chin <davidchin.yf@gmail.com>
2016-10-13 13:05:35 +1100

    CHECKOUT-973: Use integerAmount

--- src/payment/mappers/map-to-order-totals.js
+++ src/payment/mappers/map-to-order-totals.js
@@ -8,12 +8,11 @@ import { omitNil } from '../../common/utils';
 export default function mapToOrderTotals(data) {
     const { cart } = data;
 
-    // KLUDGE: amount * 100 - integerAmount is not available yet
     return omitNil({
         grand_total: cart.grandTotal ? cart.grandTotal.integerAmount : null,
-        handling: cart.handling ? cart.handling.integerAmount || cart.handling.amount * 100 : null,
-        shipping: cart.shipping ? cart.shipping.integerAmount || cart.shipping.amount * 100 : null,
-        subtotal: cart.subTotal ? cart.subTotal.integerAmount || cart.subTotal.amount * 100 : null,
-        tax: cart.taxTotal ? cart.taxTotal.integerAmount || cart.taxTotal.amount * 100 : null,
+        handling: cart.handling ? cart.handling.integerAmount : null,
+        shipping: cart.shipping ? cart.shipping.integerAmount : null,
+        subtotal: cart.subTotal ? cart.subTotal.integerAmount : null,
+        tax: cart.taxTotal ? cart.taxTotal.integerAmount : null,
     });
 }
