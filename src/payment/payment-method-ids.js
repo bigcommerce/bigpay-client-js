0720e302d30ab92e74ecdcfdbb28db32fc840646
Olga Lashkul <olga.lashkul@bigcommerce.com>
2020-04-28 11:35:39 +0300

    feat(payments): PAYPAL-293 Add paypalcommerce to payment methods

--- src/payment/payment-method-ids.js
+++ src/payment/payment-method-ids.js
@@ -3,3 +3,6 @@ export const BRAINTREE_PAYPAL = 'braintreepaypal';
 export const BRAINTREE_PAYPAL_CREDIT = 'braintreepaypalcredit';
 export const BRAINTREE_VISACHECKOUT = 'braintreevisacheckout';
 export const BRAINTREE_GOOGLEPAY = 'googlepaybraintree';
+
+export const PAYPAL_COMMERCE = 'paypalcommerce';
+export const PAYPAL_COMMERCE_CREDIT = 'paypalcommercecredit';
